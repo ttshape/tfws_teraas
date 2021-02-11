10bd0b4397abcc66e9f0d8835c766882fa3dd29c
Aurélien Pelletier <720961+toutantic@users.noreply.github.com>
2021-02-11 20:38:50 +0100

    Update main.tf

--- terraform_configurations/backendsBuckets/main.tf
+++ terraform_configurations/backendsBuckets/main.tf
@@ -2,7 +2,7 @@ module "bucket" {
   source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
    version = "1.7.2"
 
-  name       = var.bucketName
+  name       = "teraas-${var.workspaceName}"
   project_id = var.project
   location   = var.location
   versioning = true
